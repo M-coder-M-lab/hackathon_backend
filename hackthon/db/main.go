dd09aa982e54ac6ea3af95aaa2f7c9dfec0921e9
M-coder-M-lab <n_yuki0817@outlook.jp>
2025-06-20 03:14:18 +0900

    Update main.go

--- hackthon/db/main.go
+++ hackthon/db/main.go
@@ -106,7 +106,7 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", https://hackthon-51gb.vercel.app/")
+		w.Header().Set("Access-Control-Allow-Origin", "https://hackthon-51gb.vercel.app/")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
