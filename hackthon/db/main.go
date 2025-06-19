365b6d46120ab5f189ab5a180ff2d8d7db5a376f
M-coder-M-lab <n_yuki0817@outlook.jp>
2025-06-20 00:19:30 +0900

    Update main.go

--- hackthon/db/main.go
+++ hackthon/db/main.go
@@ -98,7 +98,7 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
