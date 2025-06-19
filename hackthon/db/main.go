4862a06329a8cb855c0881c16c9600c0a7a6adc4
M-coder-M-lab <n_yuki0817@outlook.jp>
2025-06-20 00:28:43 +0900

    Update main.go

--- hackthon/db/main.go
+++ hackthon/db/main.go
@@ -98,17 +98,26 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+
+		if origin == "https://hackathon-xj55.vercel.app" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Preflightリクエストへの即時レスポンス
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+
 		next.ServeHTTP(w, r)
 	})
 }
+
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	var payload struct {
